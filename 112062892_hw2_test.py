0daf63abf539bd40383511f0e0406523de73c61e
Yuhang Song <1595025647@qq.com>
2024-04-22 13:52:53 +0800

    test

--- 112062892_hw2_test.py
+++ 112062892_hw2_test.py
@@ -1,11 +1,11 @@
 import gym
+import numpy as np
 
 
 class Agent(object):
     """Agent that acts randomly."""
     def __init__(self):
-        self.action_space = gym.spaces.Discrete(12)
-        pass
+        self.action_space = gym.spaces.Box(low=0, high=1, shape=(22,), dtype=np.float32)
 
     def act(self, observation):
         return self.action_space.sample()
